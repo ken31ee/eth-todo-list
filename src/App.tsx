2bddb76fb16d81be6dbddd973bb3d7027e4cd30c
Ken Tung <ken@fano.ai>
2021-04-17 00:46:14 +0800

    core: Add material-ui dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
+import { Button } from '@material-ui/core';
 import * as React from 'react';
-import './App.css';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return <div>
+    <Button>Hello World</Button>
+    <div>abs</div>
+  </div>
 }
 
 export default App;
